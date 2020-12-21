8328a772376ab38d656092f535357848015545cc
Sujit <sjoshi1@saic.edu>
2020-12-22 03:06:44 +0530

    mat view images test

--- main.js
+++ main.js
@@ -7,18 +7,30 @@ let spWhite = '#FFFFFF';
 //======== material functions ===========
 
 function agateClick() {
-    document.getElementById('semi-precious-product-viewer').style.backgroundImage='url(./images/agates/blueagateShilaAndStyle.jpg)';
+    document.getElementById('semi-precious-product-viewer').style.backgroundImage='url(./images/agates/whiteagateShilaAndStyle.jpg)';
+}
+
+function amethystClick() {
+    document.getElementById('semi-precious-product-viewer').style.backgroundImage='url(./images/amethysts/amethysttumblesShilaAndStyle.jpg)';
+}
+
+function jasperClick() {
+    document.getElementById('semi-precious-product-viewer').style.backgroundImage='url(./images/jaspers/fancyjasperShilaAndStyle.jpg)';
+}
+
+function aventurineClick() {
+    document.getElementById('semi-precious-product-viewer').style.backgroundImage='url(./images/aventurines/redaventurineShilaAndStyle.jpg)';
 }
 
 //========= utility functions ===========
 
-function selMat(x) {
-    x.style.backgroundColor = spGold;
-    x.style.color = spWhite;
-}
-
-function deselMat(x) {
-    x.style.backgroundColor = 'unset';
-    x.style.color = 'unset';
-}
+// function selMat(x) {
+    // x.style.backgroundColor = spGold;
+    // x.style.color = spWhite;
+// }
+// 
+// function deselMat(x) {
+    // x.style.backgroundColor = 'unset';
+    // x.style.color = 'unset';
+// }
 
