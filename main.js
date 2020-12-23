801f2cd1b4819650834199bb8ea2b2ce1ac465b2
Sujit <sjoshi1@saic.edu>
2020-12-23 09:46:24 +0530

    delete sel

--- main.js
+++ main.js
@@ -19,31 +19,31 @@ function agateClick() {
     document.getElementById('semi-precious-product-viewer').style.backgroundImage='url(./images/agates/whiteagateShilaAndStyle.jpg)';
 
     document.getElementById('prod-button-1').style.display='block';
-    document.getElementById('prod-button-1').style.backgroundImage = 'url(./images/agates/whiteagateShilaAndStyle.jpg)';
+    document.getElementById('prod-button-1').style.backgroundImage = 'url(./images/thumbnails/whiteagateShilaAndStyle.jpg)';
     document.getElementById('prod-button-1').innerHTML = 'SAG 06'
 
     document.getElementById('prod-button-2').style.display='block';
-    document.getElementById('prod-button-2').style.backgroundImage = 'url(./images/agates/blueagateShilaAndStyle.jpg)';
+    document.getElementById('prod-button-2').style.backgroundImage = 'url(./images/thumbnails/blueagateShilaAndStyle.jpg)';
     document.getElementById('prod-button-2').innerHTML = 'SAG 06'
 
     document.getElementById('prod-button-3').style.display='block';
-    document.getElementById('prod-button-3').style.backgroundImage = 'url(./images/agates/greyagateShilaAndStyle.jpg)';
+    document.getElementById('prod-button-3').style.backgroundImage = 'url(./images/thumbnails/greyagateShilaAndStyle.jpg)';
     document.getElementById('prod-button-3').innerHTML = 'SAG 06'
 
     document.getElementById('prod-button-4').style.display='block';
-    document.getElementById('prod-button-4').style.backgroundImage = 'url(./images/agates/yellowagateShilaAndStyle.jpg)';
+    document.getElementById('prod-button-4').style.backgroundImage = 'url(./images/thumbnails/yellowagateShilaAndStyle.jpg)';
     document.getElementById('prod-button-4').innerHTML = 'SAG 06'
 
     document.getElementById('prod-button-5').style.display='block';
-    document.getElementById('prod-button-5').style.backgroundImage = 'url(./images/agates/crystalagateShilaAndStyle.jpg)';
+    document.getElementById('prod-button-5').style.backgroundImage = 'url(./images/thumbnails/crystalagateShilaAndStyle.jpg)';
     document.getElementById('prod-button-5').innerHTML = 'SAG 06'
 
     document.getElementById('prod-button-6').style.display='block';
-    document.getElementById('prod-button-6').style.backgroundImage = 'url(./images/agates/raniagateShilaAndStyle.jpg)';
+    document.getElementById('prod-button-6').style.backgroundImage = 'url(./images/thumbnails/raniagateShilaAndStyle.jpg)';
     document.getElementById('prod-button-6').innerHTML = 'SAG 06'
 
     document.getElementById('prod-button-7').style.display='block';
-    document.getElementById('prod-button-7').style.backgroundImage = 'url(./images/agates/greenagateShilaAndStyle.jpg)';
+    document.getElementById('prod-button-7').style.backgroundImage = 'url()';
     document.getElementById('prod-button-7').innerHTML = 'SAG 06'
 }
 
